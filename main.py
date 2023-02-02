a45e69500a4f25a0ad00724d336897bc015c839c
SlavaCPSU1 <s.i.ares@mail.ru>
2023-02-02 18:05:23 +0300

    add correct

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Bot_command import *
 
 
 
-app = ApplicationBuilder().token("6169466115:AAFTa0T2E2Ope-5KfoWmJgFHRPe8rUdqQLM").build()
+app = ApplicationBuilder().token("").build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("sum", sum_command))
